7591fa61c3d25167d00ec5e184a7463ae7c0469a
John McAleely <john@mcaleely.com>
2021-04-06 18:14:51 +0100

    end of lecture 2

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,25 @@
 //  Created by John McAleely on 06/04/2021.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var game: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in emojis[pairIndex] }
+    }
+    
+    // MARK: Access to the Model
+
+    var cards: Array<MemoryGame<String>.Card> {
+        game.cards
+    }
+    
+    // MARK: Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        game.choose(card: card)
+    }
+}
